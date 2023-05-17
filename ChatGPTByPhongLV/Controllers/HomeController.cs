8dd4e82255fddfbe228325ca2083ccbb3296ae3f
Phong <lovanphong2299@gmail.com>
2023-05-17 16:50:35 +0700

    Add project files.

---
+++ ChatGPTByPhongLV/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using ChatGPTByPhongLV.Models;
+using ChatGPTByPhongLV.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+
+namespace ChatGPTByPhongLV.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendChatGPT([FromBody] string question)
+        {
+            var apiKey = "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";
+            var apiUrl = "https://api.openai.com/v1/chat/completions"; //Thay thế bằng URL API ChatGPT của bạn
+            var client = new ChatGPTClient(apiKey, apiUrl);
+            
+            var answer = await client.GetChatResponse(question);
+            // Gọi API ChatGPT và nhận câu trả lời
+           
+            // Trả về câu trả lời dưới dạng JSON
+            return Ok(new { answer });
+        }
+
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ChatGPTByPhongLV/Repository/ChatGPTClient.cs
@@ -0,0 +1,92 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ChatGPTByPhongLV.Repository
+{
+    public class ChatGPTClient
+    {
+        private readonly HttpClient httpClient;
+        private readonly string apiKey;
+        private readonly string apiUrl;
+
+        public ChatGPTClient(string apiKey, string apiUrl)
+        {
+            this.apiKey = apiKey;
+            this.apiUrl = apiUrl;
+            this.httpClient = new HttpClient();
+            this.httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {this.apiKey}");
+        }
+
+        public async Task<string> GetChatResponse(string question)
+        {
+            var requestData = new
+            {
+                model = "gpt-3.5-turbo",
+                messages = new[]
+                {
+
+                new { role = "user", content = question }
+                      }
+            };
+
+            var jsonContent = new StringContent(Newtonsoft.Json.JsonConvert.SerializeObject(requestData), Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync(apiUrl, jsonContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                // Xử lý câu trả lời từ jsonResponse và trả về
+                // Ví dụ:
+                var answer = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(jsonResponse).choices[0].message.content.ToString();
+                return answer;
+            }
+            else
+            {
+                throw new Exception("Request failed with status code: " + response.StatusCode);
+            }
+        }
+        public void TestTransection()
+        {
+            using (SqlConnection connection = new SqlConnection("hihi"))
+            {
+                connection.Open();
+                SqlTransaction transaction = connection.BeginTransaction();
+                try
+                {
+                    // Thực thi stored procedure trong giao dịch
+                    SqlCommand command1 = new SqlCommand("sp_InsertCustomer", connection, transaction);
+                    command1.CommandType = CommandType.StoredProcedure;
+                    command1.Parameters.AddWithValue("@CustomerName", "Company Inc");
+                    command1.Parameters.AddWithValue("@ContactName", "John Doe");
+                    command1.Parameters.AddWithValue("@Country", "USA");
+                    command1.ExecuteNonQuery();
+
+                    SqlCommand command12 = new SqlCommand("sp_InsertCustomer", connection, transaction);
+                    command1.CommandType = CommandType.StoredProcedure;
+                    command1.Parameters.AddWithValue("@CustomerName", "Company Inc");
+                    command1.Parameters.AddWithValue("@ContactName", "John Doe");
+                    command1.Parameters.AddWithValue("@Country", "USA");
+                    command1.ExecuteNonQuery();
+
+                    // Hoàn thành giao dịch
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    // Xảy ra lỗi trong quá trình thực thi giao dịch, rollback giao dịch
+                    Console.WriteLine("Error occurred, rolling back transaction: " + ex.Message);
+                    try
+                    {
+                        transaction.Rollback();
+                    }
+                    catch (Exception ex2)
+                    {
+                        Console.WriteLine("Rollback failed: " + ex2.Message);
+                    }
+                }
+            }
+
+        }
+    }
+}
